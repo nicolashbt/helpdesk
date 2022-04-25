1e7af9c401d0bafb66746cdbd39cd85e20e4db8e
Nicolas Hennebert <nicolas.hbt@protonmail.com>
2022-04-25 11:50:04 +0200

    add error handler

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,23 @@
-const registerUser = (req, res) => {
+const asyncHandler = require("express-async-handler");
+
+// @route   /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Some fields are missing");
+  }
+
   res.send("register route");
-};
-const loginUser = (req, res) => {
+});
+
+// @route /api/users/login
+// @access  Public
+const loginUser = asyncHandler(async (req, res) => {
   res.send("login route");
-};
+});
 
 module.exports = {
   registerUser,
