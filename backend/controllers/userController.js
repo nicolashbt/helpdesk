a507aeffdb325f36f146ed1293f94aaa23f9fe32
Nicolas Hennebert <nicolas.hbt@protonmail.com>
2022-04-25 13:42:29 +0200

    add registerUser

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,7 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcryptjs");
+
+const User = require("../models/userModel");
 
 // @route   /api/users
 // @access  Public
@@ -10,7 +13,33 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error("Some fields are missing");
   }
 
-  res.send("register route");
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already in DB");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data')
+  }
+
 });
 
 // @route /api/users/login
