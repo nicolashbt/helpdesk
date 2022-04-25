deefe89c77087751c9b3611bb489924f7ac84dd2
Nicolas Hennebert <nicolas.hbt@protonmail.com>
2022-04-25 11:26:43 +0200

    add userController and userRoutes

---
+++ backend/controllers/userController.js
@@ -0,0 +1,11 @@
+const registerUser = (req, res) => {
+  res.send("register route");
+};
+const loginUser = (req, res) => {
+  res.send("login route");
+};
+
+module.exports = {
+  registerUser,
+  loginUser,
+};
